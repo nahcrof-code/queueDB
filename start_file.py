0658fc876eecd17a9e81178825dbd60bbcdd528a
Tyrae Paul <68835586+scuzzles@users.noreply.github.com>
2025-11-16 12:17:02 -0600

    Create start_file.py
    Basically the config

---
+++ start_file.py
@@ -0,0 +1,10 @@
+import json
+
+FILENAME = "stop.json"
+API_TOKEN = "TOKEN-HERE"
+HOST = "0.0.0.0"
+PORT = 2022
+
+def start_rule(): # allows you to set specific code to return extra things in the database at startup
+    DB = {}
+    return DB
